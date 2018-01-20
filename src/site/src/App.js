9cad0be6ea86a12ba1364e6ea90d0dc9185e2d09
Thais <thais@minet.dk>
2018-01-20 15:28:55 -0600

    wrote Login component

--- src/site/src/App.js
+++ src/site/src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import Login from "./components/Login.js";
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
-  }
-
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
-        )}
+        <h1>CCT ABET</h1>
+        <div>
+          <Login />
+        </div>
       </div>
     );
   }
 }
 
 export default App;
+
+// class App extends Component {
+//   state = {users: []}
+
+//   componentDidMount() {
+//     fetch('/users')
+//       .then(res => res.json())
+//       .then(users => this.setState({ users }));
+//   }
+
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Users</h1>
+//         {this.state.users.map(user =>
+//           <div key={user.id}>{user.username}</div>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
 
 // import React, { Component } from 'react';
 // import logo from './logo.svg';
