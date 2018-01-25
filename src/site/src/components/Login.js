47d77cffd0e334fcff17750f11db58a025241598
Thais <thais@minet.dk>
2018-01-25 12:40:08 -0600

    need to fix cors

--- src/site/src/components/Login.js
+++ src/site/src/components/Login.js
@@ -5,51 +5,76 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: '',
-            pass: '',
+            userInfo: {
+                user: '',
+                pass: '',
+            },
+            retry: false,
         };
-
-        this.handleUserChange = this.handleUserChange.bind(this);
-        this.handlePassChange = this.handlePassChange.bind(this);
+        
         this.onLoginClick = this.onLoginClick.bind(this);
-    }
-
-    handleUserChange(event) {
-        this.setState({user: event.target.value});
-    }
-
-    handlePassChange(event) {
-        this.setState({pass: event.target.value});
     }
 
     onLoginClick() {
         console.log("Login attempt");
-        
+
+        if (!this.state.user || !this.state.pass) {
+            console.log("no username or password");
+            this.setState({retry: true});
+            return;
+        }
+
         let esc = encodeURIComponent;
-        let query = Object.keys(this.state).map(k => esc(k) + '=' + esc(this.state[k])).join('&');
-        console.log(query);
+        let query = Object.keys(this.state.userInfo).map(k => esc(k) + '=' + esc(this.state[k])).join('&');
+        //var headers = new Headers();
+        //headers.append('Content-Type', 'application/json');
+        //headers.append('Accept', 'application/json');
+        //headers.append('Access-Control-Allow-Origin');
+        // console.log(query);
 
-        // fetch("/authenticate?" + query, {
-        //     method: 'GET'
-        // }).then(res => res.json())
-        // .catch(error => console.error('Error: ', error))
-
+        fetch("http://localhost:3001/authenticate?" + query, {
+            method: 'GET',
+            //headers: headers
+        }).then(res => res.json())
+        .then(json => {
+            if (json.valid) {
+                console.log("valid user info");
+                /*
+                Do something...
+                Need to use redux and react router to redirect
+                */
+            } else {
+                console.log("invalid user info")
+                this.setState({retry: true});
+            }
+        })
+        .catch(error => console.error('Error: ', error))
     }
 
     render() {
         return (
             <div id="Login">
                 <div>
-                    <input type="text" name="username" value={this.state.user} 
-                    onChange={this.handleUserChange} placeholder="Username"/>
+                    <input type="text" value={this.state.user} 
+                    onChange={event => this.setState({user: event.target.value})}
+                    placeholder="Username"/>
                 </div>
                 <div>
-                    <input type="password" name="password" value={this.state.pass} 
-                    onChange={this.handlePassChange} placeholder="Password"/>
+                    <input type="password" value={this.state.pass} 
+                    onChange={event => this.setState({pass: event.target.value})}
+                    placeholder="Password"/>
+                </div>
+                <div>
+                    <p className={this.state.retry ? "visible" : "hidden"} >
+                        Incorrect username/password
+                    </p>
                 </div>
                 <div>
                     <button onClick={this.onLoginClick}>Login</button>
                 </div>
+                {/* <div>
+                    <a href="/auth/provider">Log In with OAuth Provider</a>
+                </div> */}
             </div>
         );
     }
