10fc0452ef4755ff644b26268d263ffb74454652
Connor Austin <arcademissile@gmail.com>
2018-01-29 19:43:35 -0600

    Added stuff

---
+++ docker/docker_runtime.sh
@@ -0,0 +1,16 @@
+# The Docker container runs this on startup. See the Dockerfile
+
+# Start postgresql
+echo "Starting postgresql"
+service postgresql start
+echo "Starting site"
+
+# Start site
+echo "Starting site"
+cd /app/src/site
+PORT=3000 npm start &
+
+# Start server
+echo "Starting server"
+cd /app/src/server
+npm start
