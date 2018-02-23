3837198d1b68b3da5537489f565bdb40838061e2
Thais <thais@minet.dk>
2018-02-23 16:54:23 -0600

    resolved merge conflicts in adminReducer

--- src/site/src/reducers/adminReducer.js
+++ src/site/src/reducers/adminReducer.js
@@ -10,11 +10,8 @@ import {
 
 function adminReducer(state = {
     isDoingRequest: false,
-<<<<<<< HEAD
     createDBResponse: false,
     insertTestResponse: false,
-=======
->>>>>>> 0fe77855524741d34f67c467c5c51b9c800b7d23
     createdDB: false,
     insertedTestData: false,
     requestError: null,
@@ -27,20 +24,14 @@ function adminReducer(state = {
         case CREATE_DB_SUCCESS:
             return Object.assign({}, state, {
                 isDoingRequest: false,
-<<<<<<< HEAD
                 createDBResponse: true,
-=======
->>>>>>> 0fe77855524741d34f67c467c5c51b9c800b7d23
                 createdDB: true,
                 requestError: null,
             });
         case CREATE_DB_FAIL:
             return Object.assign({}, state, {
                 isDoingRequest: false,
-<<<<<<< HEAD
                 createDBResponse: true,
-=======
->>>>>>> 0fe77855524741d34f67c467c5c51b9c800b7d23
                 createdDB: false,
                 requestError: action.response.error,
             });
@@ -51,20 +42,14 @@ function adminReducer(state = {
         case INSERT_TEST_DATA_SUCCESS:
             return Object.assign({}, state, {
                 isDoingRequest: false,
-<<<<<<< HEAD
                 insertTestResponse: true,
-=======
->>>>>>> 0fe77855524741d34f67c467c5c51b9c800b7d23
                 insertedTestData: true,
                 requestError: null,
             });
         case INSERT_TEST_DATA_FAIL:
             return Object.assign({}, state, {
                 isDoingRequest: false,
-<<<<<<< HEAD
                 insertTestResponse: true,
-=======
->>>>>>> 0fe77855524741d34f67c467c5c51b9c800b7d23
                 insertedTestData: false,
                 requestError: action.response.error,
             });
