b916db51cc8be785a09b86518fa158d251f46a2d
Connor Austin <cjaustin42@gmail.com>
2018-03-14 14:10:40 -0500

    forms api working

--- src/server/routes/router.js
+++ src/server/routes/router.js
@@ -21,8 +21,8 @@ var forms = require('./forms');
 router.get('/', index.index);
 
 router.get('/users', auth.require_admin, users.get_users);
-router.post('/users/create', auth.require_admin, users.create_user);
 router.get('/users/delete', auth.require_admin, users.delete_user);
+router.post('/users/create', auth.require_admin, users.create_user);
 
 // secret API endpoint to create user in case of DB reset where cannot
 // insert test data
@@ -33,12 +33,13 @@ router.get('/auth/is_logged_in', auth.is_logged_in);
 router.get('/auth/logout', auth.require_login, auth.logout);
 
 router.get('/courses', auth.require_login, courses.get_courses);
-router.post('/courses/create', auth.require_login, courses.create_course);
 router.get('/courses/delete', auth.require_login, courses.delete_course);
+router.post('/courses/create', auth.require_login, courses.create_course);
 
 router.get('/forms', auth.require_login, forms.get_forms);
-router.post('/forms/create', auth.require_login, forms.create_form);
 router.get('/forms/delete', auth.require_login, forms.delete_form);
+router.post('/forms/create', auth.require_login, forms.create_form);
+router.post('/forms/update', auth.require_login, forms.update_form);
 
 router.get('/admin/create_db', auth.require_admin, admin.create_db);
 router.get('/admin/insert_test_data', auth.require_admin, admin.insert_test_data);
