b815d1fb1d8484e70d02bcf4183c60ec51280b81
toppinsb96 <31316272+toppinsb96@users.noreply.github.com>
2018-03-14 11:45:50 -0500

    Preliminary Forms complete

--- src/site/src/components/Form.js
+++ src/site/src/components/Form.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import FormList from './FormList.js';
 
 class Form extends Component {
+    constructor() {
+        super();
+        this.state = {
+            assessmentCoor: 'Arisoa',
+            semester: 'Spring 2018',
+            course: 'CS 4453 Artificial Intelligence',
+        }
+        this.updateCoordinator = this.updateCoordinator.bind(this);
+        this.updateSemester = this.updateSemester.bind(this);
+        this.updateCourse = this.updateCourse.bind(this);
+    }
+
+    updateCoordinator(event) {
+        this.setState({assessmentCoor: event.target.value});
+    }
+    updateSemester(event) {
+        this.setState({semester: event.target.value});
+    }
+    updateCourse(event) {
+        this.setState({course: event.target.value});
+    }
+
 
 
     render() {
-        return (
+        console.log(this.state);
+        return ( 
             <div>
+                <div>
+                    <input type='text' value={this.state.assessmentCoor} onChange={this.updateCoordinator} />
+                    Assessment Coodinator: 
+                </div>
+                <div>
+                    <input type='text' value={this.state.semester} onChange={this.updateSemester}/>
+                    Semester/Year:
+                </div>
+                <div>
+                    <input type='text' value={this.state.course} onChange={this.updateCourse}/>
+                    Course:
+                </div>
                 <FormList />
             </div>
         )
--- src/site/src/components/FormList.js
+++ src/site/src/components/FormList.js
@@ -6,31 +6,62 @@ class FormList extends Component {
         super();
         this.state = {
             numStudents: 0,
-            students: [
-                {studentName: '', pointsEarned: null},
-                {studentName: 'Thais', pointsEarned: 10},
-                {studentName: 'Conner', pointsEarned: -20},
-                {studentName: 'Andrew', pointsEarned: 69},
-            ],
+            students: [],
         };
+        this.addNumStudent = this.addNumStudent.bind(this);
+        this.updateStudentName = this.updateStudentName.bind(this);
+        this.updateStudentPoints = this.updateStudentPoints.bind(this);
     }
 
-    addNumStudents(event) {
-        this.setState({students: this.state.students.concat({studentName: '', pointsEarned: null})});
+    updateStudentAmount() {
+        console.log(this.state.students.length);
+        if(this.state.numStudents !== this.state.students.length) {
+            this.setState({numStudents: this.state.students.length});
+        }
     }
+
+    addNumStudent(event) {
+        this.setState({students: this.state.students.concat([{studentName: '', pointsEarned: ''}])});
+        console.log(this.state);
+    }
+    removeStudent(student) {
+        let tmp = this.state.students;
+        tmp.splice(tmp.indexOf(student), 1);
+        this.setState({tmp: tmp});
+    }
+    updateStudentName(student, event) {
+        let tmp = this.state.students;
+        tmp[tmp.indexOf(student)].studentName = event.target.value;
+        this.setState({tmp:tmp});
+    }
+    updateStudentPoints(student, event) {
+        let tmp = this.state.students;
+        tmp[tmp.indexOf(student)].pointsEarned = event.target.value;
+        this.setState({tmp:tmp});
+    }
+    /*
+    createStudentList(event) {
+        for(var i=0;i<parseInt(event.target.value, 10);i++)
+        {
+            this.addNumStudent();
+        }
+    }*/
 
     render() {
-        const studentList = this.state.students.map( student => {
-            return (<div key={student.studentName}>
-                        <input type="text" defaultValue={student.studentName}/>
-                        <input type="text" defaultValue={student.pointsEarned}/>
+        const studentList = this.state.students.map((student, idx) => {
+            return (<div key={idx}>
+                        <input type="text" value={student.studentName} onChange={this.updateStudentName.bind(this, student)} />
+                        <input type="number" value={student.pointsEarned} onChange={this.updateStudentPoints.bind(this, student)}/>
+                        <button type="button" onClick={this.removeStudent.bind(this, student)}>-</button>
                     </div>)
         });
-
+        this.updateStudentAmount();
         return (
             <div>
+                <input type="number" value={this.state.numStudents}/>
+                <button type="button" onClick={this.addNumStudent}>+</button>
+                <div>Name: Points:</div>
                 {studentList}
-                <button type="button" onClick={this.addNumStudents}>+</button>
             </div>
         )
     }
@@ -43,3 +74,11 @@ export default FormList;
 {studentName: 'Conner', pointsEarned: 20},
 {studentName: 'Andrew', pointsEarned: 9},
 */
+  /*
+  onChange={this.createStudentList.bind(this)
+    createStudentList(event) {
+        for(var i=0;i<parseInt(event.target.value, 10);i++)
+        {
+            this.addNumStudent();
+        }
+    }*/
