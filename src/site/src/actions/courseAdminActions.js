6ffc331ebec3b3501c57d45b87ce97cdeb5d4a56
Thais <thais@minet.dk>
2018-04-24 16:34:48 -0500

    added creation of courses

--- src/site/src/actions/courseAdminActions.js
+++ src/site/src/actions/courseAdminActions.js
@@ -45,6 +45,72 @@ export function courseListShowHide() {
     }
 }
 
+
+// ations for changing fields of course creation
+export const CREATE_COURSE_EMAIL_CHANGE = 'CREATE_COURSE_EMAIL_CHANGE';
+export function createCourseEmailChange(email) {
+    return {
+        type: CREATE_COURSE_EMAIL_CHANGE,
+        email
+    }
+}
+
+export const CREATE_COURSE_COURSE_NAME_CHANGE = 'CREATE_COURSE_COURSE_NAME_CHANGE';
+export function createCourseNameChange(course_name) {
+    return {
+        type: CREATE_COURSE_COURSE_NAME_CHANGE,
+        course_name,
+    }
+}
+
+export const CREATE_COURSE_SEMESTER_CHANGE = 'CREATE_COURSE_SEMESTER_CHANGE';
+export function createCourseSemesterChange(semester) {
+    return {
+        type: CREATE_COURSE_SEMESTER_CHANGE,
+        semester,
+    }
+}
+
+export const CREATE_COURSE_YEAR_CHANGE = 'CREATE_COURSE_YEAR_CHANGE';
+export function createCourseYearChange(year) {
+    return {
+        type: CREATE_COURSE_YEAR_CHANGE,
+        year,
+    }
+}
+
+export const CREATE_COURSE_CLEAR = 'CREATE_COURSE_CLEAR';
+export function createCourseClear() {
+    return {
+        type: CREATE_COURSE_CLEAR,
+    }
+}
+
+export const REQUEST_CREATE_COURSE = 'REQUEST_CREATE_COURSE';
+export function requestCreateCourse(body) {
+    return {
+        type: REQUEST_CREATE_COURSE,
+        body,
+    }
+}
+
+export const CREATE_COURSE_SUCCESS = 'CREATE_COURSE_SUCCESS';
+export function createCourseSuccess(response) {
+    return {
+        type: CREATE_COURSE_SUCCESS,
+        response,
+    }
+}
+
+export const CREATE_COURSE_FAIL = 'CREATE_COURSE_FAIL';
+export function createCourseFail(response) {
+    return {
+        type: CREATE_COURSE_FAIL,
+        response,
+    }
+}
+
+
 export function getCourseList(semester = null, year = null) {
     let query = '';
     if (semester && !year) {
@@ -66,6 +132,35 @@ export function getCourseList(semester = null, year = null) {
                     dispatch(courseListSuccess(json));
                 } else {
                     dispatch(courseListFail(json));
+                }
+            });
+    }
+}
+
+export function createCourse(courseCreate) {
+    let body = {
+        course_name: courseCreate.course_name,
+        email: courseCreate.email,
+        semester: courseCreate.semester,
+        year: courseCreate.year,
+    }
+
+    return dispatch => {
+        dispatch(requestCreateCourse(body));
+        return fetch('/courses/create', {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(body),
+        }).then(res => res.json())
+            .then(json => {
+                if (!json.error) {
+                    dispatch(createCourseSuccess(json));
+                    dispatch(createCourseClear());
+                } else {
+                    dispatch(createCourseFail(json));
                 }
             });
     }
--- src/site/src/reducers/courseAdminReducer.js
+++ src/site/src/reducers/courseAdminReducer.js
@@ -4,11 +4,20 @@ import {
     COURSE_LIST_FAIL,
     COURSE_LIST_FILTER_SEMESTER_CHANGE,
     COURSE_LIST_FILTER_YEAR_CHANGE,
-    COURSE_LIST_SHOW_HIDE
+    COURSE_LIST_SHOW_HIDE,
+    CREATE_COURSE_COURSE_NAME_CHANGE,
+    CREATE_COURSE_EMAIL_CHANGE,
+    CREATE_COURSE_SEMESTER_CHANGE,
+    CREATE_COURSE_YEAR_CHANGE,
+    CREATE_COURSE_CLEAR,
+    REQUEST_CREATE_COURSE,
+    CREATE_COURSE_SUCCESS,
+    CREATE_COURSE_FAIL,
 } from '../actions/courseAdminActions.js';
 
 function courseAdminReducer(state = {
     isDoingRequest: false,
+    requestMessage: null,
     requestError: null,
     courseList: null,
     showHide: false,
@@ -16,21 +25,30 @@ function courseAdminReducer(state = {
         semester: '',
         year: ''
     },
+    courseCreate: {
+        email: '',
+        course_name: '',
+        semester: '',
+        year: '',
+    },
 }, action) {
     switch(action.type) {
         case REQUEST_COURSE_LIST:
             return Object.assign({}, state, {
                 isDoingRequest: true,
+                requestMessage: null,
                 requestError: null,
             });
         case COURSE_LIST_SUCCESS:
             return Object.assign({}, state, {
                 isDoingRequest: false,
+                requestMessage: action.response.message,
                 courseList: action.response.courseData,
             });
         case COURSE_LIST_FAIL:
             return Object.assign({}, state, {
                 isDoingRequest: false,
+                requestMessage: null,
                 requestError: action.response.error,
             });
         case COURSE_LIST_FILTER_SEMESTER_CHANGE:
@@ -51,6 +69,69 @@ function courseAdminReducer(state = {
             return Object.assign({}, state, {
                 showHide: !state.showHide,
             });
+        case CREATE_COURSE_COURSE_NAME_CHANGE:
+            return Object.assign({}, state, {
+                courseCreate: {
+                    email: state.courseCreate.email,
+                    course_name: action.course_name,
+                    semester: state.courseCreate.semester,
+                    year: state.courseCreate.year,
+                }
+            });
+        case CREATE_COURSE_EMAIL_CHANGE:
+            return Object.assign({}, state, {
+                courseCreate: {
+                    email: action.email,
+                    course_name: state.courseCreate.course_name,
+                    semester: state.courseCreate.semester,
+                    year: state.courseCreate.year,
+                }
+            });
+        case CREATE_COURSE_SEMESTER_CHANGE:
+            return Object.assign({}, state, {
+                courseCreate: {
+                    email: state.courseCreate.email,
+                    course_name: state.courseCreate.course_name,
+                    semester: action.semester,
+                    year: state.courseCreate.year,
+                }
+            });
+        case CREATE_COURSE_YEAR_CHANGE:
+            return Object.assign({}, state, {
+                courseCreate: {
+                    email: state.courseCreate.email,
+                    course_name: state.courseCreate.course_name,
+                    semester: state.courseCreate.semester,
+                    year: action.year,
+                }
+            });
+        case CREATE_COURSE_CLEAR:
+            return Object.assign({}, state, {
+                courseCreate: {
+                    email: '',
+                    course_name: '',
+                    semester: '',
+                    year: '',
+                }
+            });
+        case REQUEST_CREATE_COURSE:
+            return Object.assign({}, state, {
+                isDoingRequest: true,
+                requestMessage: null,
+                requestError: null,
+            });
+        case CREATE_COURSE_SUCCESS:
+            return Object.assign({}, state, {
+                isDoingRequest: true,
+                requestMessage: action.response,
+                requestError: null,
+            });
+        case CREATE_COURSE_FAIL:
+            return Object.assign({}, state, {
+                isDoingRequest: false,
+                requestMessage: null,
+                requestError: action.response.error,
+            })
         default:
             return state;
     }
