8fa6cbf3f6a4624bb5a331fdf749333460f68048
Thais <thais@minet.dk>
2018-04-27 20:21:33 -0500

    added new form ability to administration of forms

--- src/site/src/components/FormAdmin.js
+++ src/site/src/components/FormAdmin.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
     updateFormItemCompleted,
-    updateFormItemOutcome
+    updateFormItemOutcome,
+    updateNewFormCompleted,
+    updateNewFormOutcome,
+    addForm
 } from '../actions/formAdminActions.js';
 
 class FormAdmin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.onAddForm = this.onAddForm.bind(this);
+    }
+
     onOutcomeChange(idx, event) {
         console.log('idx: ', idx, 'event: ', event.target.value);
         this.props.updateOutcome(idx, event.target.value);
@@ -18,16 +27,32 @@ class FormAdmin extends Component {
         this.props.updateCompleted(idx, completed);
     }
 
+    onAddForm() {
+        console.log('adding form to list', this.props.newForm);
+
+        let form = {
+            form_id: null,
+            course_id: this.props.course_id,
+            outcome: this.props.newForm.outcome,
+            completed: this.props.newForm.completed,
+            data: {},
+        }
+        this.props.addForm(form);
+    }
+
     render() {
         let forms = this.props.forms.map((form, idx) => {
             return (
                 <div key={idx}>
                     <b>Outcome:</b>
                     <input type="text" value={form.outcome}
-                        onChange={this.onOutcomeChange.bind(this, idx)}/>
+                        onChange={this.onOutcomeChange.bind(this, idx)}
+                    />
                     <b>Completed:</b>
                     <input type="checkbox" checked={form.completed ? true : false}
-                        onChange={this.onCompletedChange.bind(this, idx)} />
+                        onChange={this.onCompletedChange.bind(this, idx)}
+                    />
+                    <button>Delete</button>
                 </div>
             );
         });
@@ -35,14 +60,26 @@ class FormAdmin extends Component {
         return (
             <div>
                 {forms}
+                <div>
+                    <b>Outcome:</b>
+                    <input type="text" value={this.props.newForm.outcome}
+                        onChange={event => this.props.updateNewOutcome(event.target.value)} />
+                    <b>Completed:</b>
+                    <input type="checkbox" checked={this.props.newForm.completed ? true : false}
+                        onChange={event => this.props.updateNewCompleted(
+                            this.props.newForm.completed ? 0 : 1
+                        )} />
+                    <button onClick={this.onAddForm}>Add form</button>
+                 </div>
             </div>
-        )
+        );
     }
 }
 
 const mapStateToProps = state => {
     return {
         forms: state.formAdmin.forms,
+        newForm: state.formAdmin.newForm
     }
 }
 
@@ -53,6 +90,16 @@ const mapDispatchToProps = dispatch => {
         },
         updateOutcome: (idx,outcome) => {
             dispatch(updateFormItemOutcome(idx, outcome));
+        },
+        updateNewCompleted: (completed) => {
+            console.log('blah', completed);
+            dispatch(updateNewFormCompleted(completed));
+        },
+        updateNewOutcome: (outcome) => {
+            dispatch(updateNewFormOutcome(outcome));
+        },
+        addForm: (form) => {
+            dispatch(addForm(form));
         }
     }
 }
